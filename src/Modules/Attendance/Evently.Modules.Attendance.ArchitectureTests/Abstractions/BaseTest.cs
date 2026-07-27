e850f4857665adc3c7ac75deb14096c7005ff481
darkosimon <darkosimon@yahoo.com>
2026-07-27 23:02:18 +0200

    Architecture tests for all modules

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Evently.Modules.Attendance.Domain.Attendees;
+using Evently.Modules.Attendance.Infrastructure;
+
+namespace Evently.Modules.Attendance.ArchitectureTests.Abstractions;
+
+#pragma warning disable CA1515 // Consider making public types internal
+public abstract class BaseTest
+#pragma warning restore CA1515 // Consider making public types internal
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(Attendance.Application.AssemblyReference).Assembly;
+
+    protected static readonly Assembly DomainAssembly = typeof(Attendee).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly = typeof(AttendanceModule).Assembly;
+
+    protected static readonly Assembly PresentationAssembly = typeof(Attendance.Presentation.AssemblyReference).Assembly;
+}
